c3618b4985b9b51ec8c65bd2d101b522cfd9ed84
BreatFR <6360676+breatfr@users.noreply.github.com>
2024-02-12 18:23:32 +0100

    Update mygirl.tech_new_features_v1.xx.user.js

--- mygirl.tech_new_features_v1.xx.user.js
+++ mygirl.tech_new_features_v1.xx.user.js
@@ -5,6 +5,7 @@
 // @version      1.00
 // @author       BreatFR
 // @copyright    2023, BreatFR (https://breat.fr)
+// @updateURL    https://raw.githubusercontent.com/breatfr/MyGirl/main/mygirl.tech_new_features_v1.xx.user.js
 // @match        https://app.mygirl.tech/*
 // @icon         https://app.mygirl.tech/images/logo.png
 // ==/UserScript==
